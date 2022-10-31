11a3b22e6f32fe5ddf20e9a49510388d24440b73
wanghongshuo <344734242@qq.com>
2022-10-31 20:12:33 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AgoraRTC from "agora-rtc-sdk";
+
+// appid：0b6ad43dafc944209fbf74426128086e
+// apptoken：房间列表session中agora_token字段
+// uid：99999999
+const test = () => {
+  console.log("test");
+  let client = AgoraRTC.createClient({
+    mode: "rtc",
+    codec: "vp8",
+  });
+  client.init("0b6ad43dafc944209fbf74426128086e");
+  // let remoteStream = AgoraRTC.createStream(streamSpec)
+  client.join(
+    "0060b6ad43dafc944209fbf74426128086eIACRRNBgxVIXBCKD1fBfpvejaRfbxMWnUrvp+4DO3J6AZQFqBKmqlwe6EACPDOEBUutgYwEAAQAAAAAA",
+    "",
+    "99999999"
+  );
+  // remoteStream.play('elementID')
+  // client.leave()
+};
+</script>
+
+<template>
+  <div id="elementID"></div>
+  <button @click="test">进入房间</button>
+</template>
